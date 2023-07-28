2344dca9c95127048f05aaa9d7c2d171888993bb
Lucas Miguel <lucassalles28@hotmail.com>
2023-07-28 11:53:54 -0300

    28-07-23
    update

--- aula86.py
+++ aula86.py
@@ -1,28 +1,38 @@
-# Dictonary Comprehension e Set Comprehension
+# Dictionary Comprehension e Set Comprehension
+# É a mesma coisa de List Comprehension, mas a gente troca os Colchetes '[]', pelas chaves '{}'
+# Parênteses    ()
+# Colchetes     []
+# Chaves        {}
+# Map, retorna uma lista do mesmo tamanho, com valores diferentes, Filtro retorna uma lista com elementos a menos, filtrados.
 
+
+# 1)
+print('1) Exemplo de Dict Comprehension: ')
 produto = {
-    'nome':'Caneta Azul',
-    'preço': 2.5,
-    'categoria':'Escritório',
+    'Nome':'Caneta azul',
+    'Preço':'2.50',
+    'Categoria':'Escritório',
 }
 
 
-# for valor, chave in produto.items():
-#     print(valor,chave)
+# dict_comprehension = {                                                        # Com quebra de linha
+#     chave:valor.upper()                                                       # Modificando o 'valor' para maiúsculo 
+#     if isinstance(valor, str) else valor                                      # Map: Se 'valor' for 'str' ele vai deixar maiúsculo, se não vai retornar somente o valor 
+#     for chave, valor                                                          # For vai percorrer em 'produto.items()'
+#     in produto.items()
+#     if chave != 'Categoria'                                                   # Filter: se 'chave' for diferentede 'categoria'
+# }
+
+# Map: if instancia de 'valor' for 'str', 'valor.upper()' se não retorna 'valor'.  Filtragem: if 'chave' for diferente, não mostra
+dict_comprehension = {chave:valor.upper() if isinstance(valor,str) else valor for chave, valor in produto.items() if chave != 'Categoria'}              # Sem quebra de linha
+print(dict_comprehension)
+print('\n'*2)
 
 
-# Dictonary Comprehension
-dc = {
-    chave: valor.upper()                                                        # Modificando o 'valor' para maiúsculo 
-    if isinstance(valor, str) else valor                                        # Se 'valor' for 'str' ele vai deixar maiúsculo, se não vai retornar somente o valor 
-    for chave, valor 
-    in produto.items()
-    if chave != 'categoria'                                                     # filter | se 'chave' for diferentede 'categoria'
-}
-
-# print(dc)
 
 
+# 2)
+print('2) Aqui criamos um novo dict, a partir de uma lista que parece um dict: ')
 
 lista = [
     ('a','valor a'),
@@ -30,30 +40,24 @@ lista = [
     ('c','valor c'),
 ]
 
-# print(lista)
+# dc = {                                   # Transformando a 'lista' em 'dict'
+#     chave: valor
+#     for chave, valor in lista
+# }
 
-dc = {                                              # Transformamos uma lista em dict 
-    chave: valor
-    for chave, valor in lista
-}
-# print(dc)
 
-print(dict(lista))                                  # Forma mais simples de transformar list em dict, desde que seja no modelo 'chave' 'valor'
+print(dict(lista))                         # Transformando a 'lista' em 'dict' de uma forma mais simples. Mas a lista precisa ter esse modelo de chave e valor, para funcionar.
+print('\n'*2)
+
+
+
+
+# 3)
+print('3) Set Comprehension: ')
+
+set_01 = {i*2 for i in range(10)}
+print(set_01)
 print()
 
-
-
-# Set Comprehension
-s1 = {2 ** i for i in range(10)}
-print(s1)
 # OR 
-# print(set(range(10)))
-
-
-# {teste @gmail.c}
-
-
-
-# () tupple
-# [] list
-# {} dict
+print(set(range(10)))
--- aula87.py
+++ aula87.py
@@ -1,41 +1,37 @@
-# isinstace - para saber se o objeto é de determinado tipo 
-# Em python existe uma função chamda isinstance 
-# isinstance significa é instancia de: 
+# 'isinstace' - serve para saber se um objeto é de um determinado tipo
+# 'elif' é usado para ter reservado um 'else' como default
 # Evitar misturar tipos de dados na lista
 # Sempre tantar manter as listas ou tuplas em um padrão do mesmo tipo de dados. Exemplo, uma lista de números é para tentar manter só números
 # Se usarmos string dentro daquela lista, vamos ter que usar o 'if' e pode quebrar o programa. 
-
-
-lista = [                                               # Não é uma boa prática de programação, ter um tipo de lista misturada
-    'a', 1, 1.1, True, [0,1,2],(1,2),
-    {0,1}, {'nome':'Luiz'},
+lista = [
+    'a', 1, 1.1, True, [0,1,2],(1,2),{0,1},{'nome':'luiz'}
 ]
-
+# print(type(lista[7]))
 
 
 
 for item in lista:
-    # Verifica se 'item' é instância de 'set()'. Se 'item' for instância de set(), 'item' add(5) e imprime na tela os valores que são set() e o valor boolean
-    if isinstance(item, set):
-        print('SET')
+# Verifica se 'item' é instância de 'string'. Se 'item' for instância de 'string', 'item.upper()' deixa em maiúsculo o que for string
+    if isinstance(item,str):
+        print('Str:',item.upper(), isinstance(item,str))
+        print()
+
+# Verifica se 'item' é instância de 'set()'. Se 'item' for instância de set(), 'item' add(5) e imprime na tela os valores que são set() e o valor boolean
+    elif isinstance(item,set):
+        print()
         item.add(5)
-        print(item, isinstance(item, set))
+        print('Set:',item, isinstance(item, set))
 
+# Verifica se 'item' é instância de 'int' ou 'float' e imprime 'item', e o 'item*2'
+    elif isinstance(item,(float,int)):
+        print('Int or Float',item, item*2)
+        print('-'*20)
 
-    # Verifica se 'item' é instância de 'string'. Se 'item' for instância de 'string', 'item.upper()' deixa em maiúsculo o que for string
-    elif isinstance(item, str):
-        print('STR')
-        print(item.upper())
-
-
-    # Verifica se 'item' é instância de 'int' ou 'float' e imprime 'item', e o 'item*2'
-    elif isinstance(item, (int, float)):
-        print('NUM')
-        print(item, item *2)
-    
     else:
-        print('OUTRO')
+        print()
+        print('Outro:')
         print(item)
+
 
 
 
